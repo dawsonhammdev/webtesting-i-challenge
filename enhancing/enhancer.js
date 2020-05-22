460f9e830fe1160bcce2c97db3e8947f055f753a
dawsonhammdev <hammsdesigns@gmail.com>
2020-05-21 20:31:10 -0500

    I have added all three of the tests successfully

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -5,22 +5,45 @@ module.exports = {
   get,
 };
 
+// function succeed(item) {
+//   if (item.enhancement >= 0 && item.enhancement < 20) {
+//     console.log("increase")
+//     return item.enhancement + 1;
+//   } else if (item.enhancement == 20) {
+//     console.log("changed")
+//     return item.enhancement
+//   } else if (item.enhancement < 0 && item.enhancement > 20) {
+//     console.log("not valid")
+//     return null
+//   }
+// }
+
 function succeed(item) {
-  if (item.enhancement >= 0 && item.enhancement < 20) {
-    console.log("increase")
-    return item.enhancement + 1;
-  } else if (item.enhancement == 20) {
-    console.log("changed")
-    return item.enhancement
-  } else if (item.enhancement < 0 && item.enhancement > 20) {
-    console.log("not valid")
-    return null
+  if (item.enhancement < 20 ) {
+    let enhanced = ++item.enhancement
+    return {...item, enhancement: enhanced}
+  } else {
+    return {...item, enhancement: 20}
   }
 }
 
 function fail(item) {
-  return { ...item };
+  let durable
+  let enhance
+  if (item.enhancement < 15 ) {
+    durable = item.durability - 5
+    return {...item, durability: durable}
+  } else if (item.enhancement >= 15) {
+    durable = item.durability - 10
+      if(item.enhancement > 16) {
+        enhance = item.enhancement - 1
+        return {...item, durability: durable, enhancement: enhance}
+      }
+      return {...item, durability: durable}
+  } 
 }
+
+// If the item's enhancement is less than 15, the durability of the item is decreased by 5.
 
 function repair(item) {
   if (item.enhancement === 100) {
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,28 +1,38 @@
-const enhancer = require('./enhancer.js');
+const {succeed, fail, repair, get} = require('./enhancer.js');
 // test away!
 const item = {
     name: "Chest Plate",
     durability: 47,
-    enhancement: 20
+    enhancement: 15
 }
 
 //succeed
 describe("Enhancer", () => {
-    describe("Succeed()", () => {
-        it("should return a new item object modified according to the rules for success", () => {
-            expect(() => {
-                succeed(item).toBe(item.enhancement == 9);
-            }) 
-            expect(() => {
-                succeed(item).notToBe({ enhancement: 3 })
-            })
-            // console.log("item enhancement level", succeed(item))
+    // describe("Succeed()", () => {
+    //     it("should return a new item object modified according to the rules for success", () => {
+    //         expect(() => {
+    //             succeed(item).toBe(item.enhancement === 26);
+    //         }) 
+    //         // expect(() => {
+    //         //     succeed(item).notToBe({ enhancement: 3 })
+    //         // })
+    //         // console.log("item enhancement level", succeed(item))
+    //     })
+    // })
+
+    describe("Succeed", () => {
+
+        it("if enhancement is greater than 20 then reset to 20", () => {
+            expect(succeed({ name: "Chest Plate", durability: 47, enhancement: 21 }))
+            .toStrictEqual({ name: "Chest Plate", durability: 47, enhancement: 20 })
+        })
+        it("if enhancement is less than 20 then increment by 1", () => {
+            expect(succeed(item).enhancement).toBe(16)
+
         })
     })
-})
 
-//repair
-describe('Enhancer', () => {
+    //repair
     describe('Repair()', () => {
         it('should return item object with durability of 100', () => {
             expect(() => {
@@ -30,4 +40,28 @@ describe('Enhancer', () => {
             }) 
         })
     })
+
+    //fail
+    describe("Fail", () => {
+
+        it("If the item's enhancement is less than 15, the durability of the item is decreased by 5", () => {
+            expect(fail({ name: "Chest Plate", durability: 47, enhancement: 10 }))
+            .toStrictEqual({ name: "Chest Plate", durability: 42, enhancement: 10 })
+        })
+
+        it("If the item's enhancement is 15 or more, the durability of the item is decreased by 10", () => {
+            expect(fail({ name: "Chest Plate", durability: 47, enhancement: 16 }))
+            .toStrictEqual({ name: "Chest Plate", durability: 37, enhancement: 16 })
+        })
+
+        it("If the item's enhancement level is greater than 16, the enhancement level decreases by 1 (17 goes down to 16, 18 goes down to 17)", () => {
+            expect(fail({ name: "Chest Plate", durability: 47, enhancement: 17 }))
+            .toStrictEqual({ name: "Chest Plate", durability: 37, enhancement: 16 })
+        })
+
+    })
+
 })
+
+
+
